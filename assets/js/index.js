0cd2a68ab9e92650ab604079ca3471246d316abc
ronnielopez <ronnielopez503@gmail.com>
2020-12-13 20:14:08 -0600

    añadido

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+$('#maritimo').click(function(){
+    document.getElementById('txtMaritimo').style.display = 'block';
+    document.getElementById('maritimo').style.display = 'none';
+    document.getElementById('cardMaritimo').style.height = 'auto';
+})
+
+$('#terrestre').click(function(){
+    document.getElementById('txtTerrestre').style.display = 'block';
+    document.getElementById('terrestre').style.display = 'none';
+    document.getElementById('cardTerrestre').style.height = 'auto';
+})
+
+$('#aereo').click(function(){
+    document.getElementById('txtAereo').style.display = 'block';
+    document.getElementById('aereo').style.display = 'none';
+    document.getElementById('cardAereo').style.height = 'auto';
+})
+
+$('#maritimo2').click(function(){
+    document.getElementById('txtMaritimo2').style.display = 'block';
+    document.getElementById('maritimo2').style.display = 'none';
+    document.getElementById('cardMaritimo2').style.height = 'auto';
+})
+
+$('#terrestre2').click(function(){
+    document.getElementById('txtTerrestre2').style.display = 'block';
+    document.getElementById('terrestre2').style.display = 'none';
+    document.getElementById('cardTerrestre2').style.height = 'auto';
+})
