4107142b1299280333b6ee075e897e1dbae51334
ronnielopez <ronnielopez503@gmail.com>
2020-12-15 21:10:32 -0600

    Efectos y otras cosas mas

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,14 +5,18 @@ let terrestre2 = 0;
 let aereo = 0;
 $('#maritimo').click(function () {
     if (banderaM === 0) {
-        document.getElementById('txtMaritimo').style.display = 'block';
+        $("#txtMaritimo").fadeIn();
         document.getElementById('maritimo').innerHTML = 'Ocultar';
         document.getElementById('cardMaritimo').style.height = 'auto';
+        document.getElementById('cardMaritimo').style.border = 'solid';
+        document.getElementById('cardMaritimo').style.borderColor = 'white';
+        document.getElementById('cardMaritimo').style.borderRadius = '50px';
         banderaM = 1;
     } else {
-        document.getElementById('txtMaritimo').style.display = 'none';
+        $("#txtMaritimo").fadeOut();
+        document.getElementById('cardMaritimo').style.height = 'auto';
         document.getElementById('maritimo').innerHTML = 'Ver mas';
-        document.getElementById('cardMaritimo').style.height = '200px';
+        document.getElementById('cardMaritimo').style.border = 'none';
         banderaM = 0;
     }
 
@@ -20,28 +24,36 @@ $('#maritimo').click(function () {
 
 $('#terrestre').click(function () {
     if (terrestre === 0) {
-        document.getElementById('txtTerrestre').style.display = 'block';
+        $("#txtTerrestre").fadeIn('slow');
         document.getElementById('terrestre').innerHTML = 'Ocultar';
         document.getElementById('cardTerrestre').style.height = 'auto';
+        document.getElementById('cardTerrestre').style.border = 'solid';
+        document.getElementById('cardTerrestre').style.borderColor = 'white';
+        document.getElementById('cardTerrestre').style.borderRadius = '50px';
         terrestre = 1;
     } else {
-        document.getElementById('txtTerrestre').style.display = 'none';
+        $("#txtTerrestre").fadeOut();
+        document.getElementById('cardTerrestre').style.height = 'auto';
         document.getElementById('terrestre').innerHTML = 'Ver mas';
-        document.getElementById('cardTerrestre').style.height = '200px';
+        document.getElementById('cardTerrestre').style.border = 'none';
         terrestre = 0;
     }
 })
 
 $('#aereo').click(function () {
     if (aereo === 0) {
-        document.getElementById('txtAereo').style.display = 'block';
+        $("#txtAereo").fadeIn('slow');
         document.getElementById('cardAereo').style.height = 'auto';
         document.getElementById('aereo').innerHTML = 'Ocultar';
+        document.getElementById('cardAereo').style.border = 'solid';
+        document.getElementById('cardAereo').style.borderColor = 'white';
+        document.getElementById('cardAereo').style.borderRadius = '50px';
         aereo = 1;
     } else {
-        document.getElementById('txtAereo').style.display = 'none';
+        $("#txtAereo").fadeOut();
         document.getElementById('cardAereo').style.height = 'auto';
         document.getElementById('aereo').innerHTML = 'Ver mas';
+        document.getElementById('cardAereo').style.border = 'none';
         aereo = 0;
     }
 
@@ -49,14 +61,18 @@ $('#aereo').click(function () {
 
 $('#maritimo2').click(function () {
     if (banderaM2 === 0) {
-        document.getElementById('txtMaritimo2').style.display = 'block';
+        $("#txtMaritimo2").fadeIn();
         document.getElementById('maritimo2').innerHTML = 'Ocultar';
         document.getElementById('cardMaritimo2').style.height = 'auto';
+        document.getElementById('cardMaritimo2').style.border = 'solid';
+        document.getElementById('cardMaritimo2').style.borderColor = 'white';
+        document.getElementById('cardMaritimo2').style.borderRadius = '50px';
         banderaM2 = 1;
     } else {
-        document.getElementById('txtMaritimo2').style.display = 'none';
+        $("#txtMaritimo2").fadeOut();
+        document.getElementById('cardMaritimo2').style.height = 'auto';
         document.getElementById('maritimo2').innerHTML = 'Ver mas';
-        document.getElementById('cardMaritimo2').style.height = '200px';
+        document.getElementById('cardMaritimo2').style.border = 'none';
         banderaM2 = 0;
     }
 
@@ -64,14 +80,19 @@ $('#maritimo2').click(function () {
 
 $('#terrestre2').click(function () {
     if (terrestre2 === 0) {
-        document.getElementById('txtTerrestre2').style.display = 'block';
+        $("#txtTerrestre2").fadeIn();
         document.getElementById('terrestre2').innerHTML = 'Ocultar';
         document.getElementById('cardTerrestre2').style.height = 'auto';
+        document.getElementById('cardTerrestre2').style.border = 'solid';
+        document.getElementById('cardTerrestre2').style.borderColor = 'white';
+        document.getElementById('cardTerrestre2').style.borderRadius = '50px';
         terrestre2 = 1;
     } else {
-        document.getElementById('txtTerrestre2').style.display = 'none';
+        $("#txtTerrestre2").fadeOut();
+        document.getElementById('cardTerrestre2').style.height = 'auto';
         document.getElementById('terrestre2').innerHTML = 'Ver mas';
-        document.getElementById('cardTerrestre2').style.height = '200px';
+        document.getElementById('cardTerrestre2').style.border = 'none';
         terrestre2 = 0;
     }
 })
+
